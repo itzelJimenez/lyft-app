7043409154155a0cf33f960c7abc0cde79ba2398
Itzel <itzeljimenez.do@gmail.com>
2017-06-07 12:51:02 +0000

    reformulación de código

--- js/inicio.js
+++ js/inicio.js
@@ -1,11 +1,18 @@
-$(document).ready(start);
+(function(){
 
-function start(){
+var cargarPagina = function(){
+	start();
+}
+
+var start = function(){
   setTimeout(mostrarPortada, 7100);
 }
 
-function mostrarPortada(){
+var mostrarPortada = function(){
   $('.portada').css('background-image', 'url(../imgs/portada.png)');
   $('.btn-registro').css('border-color', '#08b4ad').css('color', '#08b4ad');
-
 }
+
+$(document).ready(cargarPagina);
+
+}) ();
